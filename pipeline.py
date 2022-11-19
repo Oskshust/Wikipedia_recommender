e99c1d619c09ef847679a343e8553a0f26c7c49c
krystianMoras <krystian.moras@student.put.poznan.pl>
2022-11-19 10:48:39 +0100

    50 articles pulled

--- pipeline.py
+++ pipeline.py
@@ -12,31 +12,33 @@ import pandas as pd
 from nltk.tokenize import word_tokenize, wordpunct_tokenize, sent_tokenize
 
 porter = PorterStemmer()
-
+wordnet = WordNetLemmatizer()
 COLUMNS = ["title","summary","links","categories","references"] # todo: add sections
 
 def download_article():
     try:
         return wikipedia.page(title=wikipedia.random(1))
-    except:
+    except: #dziwnie ta paczka działa, czasem artykułu nie ma z tytułem który został wylosowany
         return download_article()
-
 
 def preprocess_article(article):
 
+    title = article.title
+    summary = wordnet.lemmatize(porter.stem(article.summary))
+    categories = "|".join(article.categories)
+    links = "|".join(article.links)
     try:
         references = "|".join(article.references)
     except KeyError:
         references = ""
-    preprocessed_dict = {"title":article.title,"summary":porter.stem(article.summary),"links":"|".join(article.links),"categories":"|".join(article.categories),"references":references}
 
     try:
         df = pd.read_csv(open("wikipedia.csv",encoding="utf-8"),encoding_errors='ignore').set_index("title")
-
-        df.loc[article.title] = (porter.stem(article.summary),"|".join(article.links),"|".join(article.categories),references)
+        df.loc[title] = (summary,links,categories,references)
     except pd.errors.EmptyDataError as e:
+        preprocessed_dict = {"title":title,"summary":summary,"links":links,"categories":categories,"references":references}
         df = pd.DataFrame.from_records([preprocessed_dict],index="title")
-    print(df)
+
     df.to_csv(open("wikipedia.csv",mode="w",encoding="utf-8"))
 
 def pipeline(iterations):
@@ -44,6 +46,5 @@ def pipeline(iterations):
         article = download_article()
         print(article.title)
         preprocess_article(article)
-# title | contents | links | categories |
 
-pipeline(5)
+pipeline(50)
