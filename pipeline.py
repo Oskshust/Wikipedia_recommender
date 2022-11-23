c24311b96e7f6a511e5bb1cba742142efbb43a17
krystianMoras <krystian.moras@student.put.poznan.pl>
2022-11-23 23:07:45 +0100

    add conent based recommendation

--- pipeline.py
+++ pipeline.py
@@ -1,50 +1,48 @@
 import wikipedia
-import asyncio
-
-import nltk
 
 from nltk.stem import PorterStemmer
-from nltk.stem import LancasterStemmer
 from nltk.stem import WordNetLemmatizer
-from nltk.corpus import stopwords
-import re
 import pandas as pd
 from nltk.tokenize import word_tokenize, wordpunct_tokenize, sent_tokenize
-
+from nltk.corpus import stopwords
 porter = PorterStemmer()
 wordnet = WordNetLemmatizer()
-COLUMNS = ["title","summary","links","categories","references"] # todo: add sections
+COLUMNS = ["title","summary","content","links","categories","references"] # todo: add sections
 
 def download_article():
     try:
         return wikipedia.page(title=wikipedia.random(1))
-    except: #dziwnie ta paczka działa, czasem artykułu nie ma z tytułem który został wylosowany; Oskar: tak, no właśnie też się na to natknąłem przy tych próbach pobierania w jupyterze, ale except continue jakoś mi tam załatwiało robotę to tak zostawiłem
+    except: # sometimes the package can't find the article but the title is a random title from the list of articles
         return download_article()
+
 
 def preprocess_article(article):
 
     title = article.title
     summary = wordnet.lemmatize(porter.stem(article.summary))
+    content =wordnet.lemmatize(porter.stem(article.content))
+    content = " ".join([word for word in content.split() if word not in set(stopwords.words('english'))])
+
     categories = "|".join(article.categories)
     links = "|".join(article.links)
     try:
         references = "|".join(article.references)
     except KeyError:
         references = ""
-
+    return title,summary,content,links,categories,references
+def save_article(title,summary,content,links,categories,references):
     try:
-        df = pd.read_csv(open("wikipedia.csv",encoding="utf-8"),encoding_errors='ignore').set_index("title")
-        df.loc[title] = (summary,links,categories,references)
+        df = pd.read_csv(open("wikipedia2.csv",encoding="utf-8"),encoding_errors='ignore').set_index("title")
+        df.loc[title] = (summary,content,links,categories,references)
     except pd.errors.EmptyDataError as e:
-        preprocessed_dict = {"title":title,"summary":summary,"links":links,"categories":categories,"references":references}
+        preprocessed_dict = {"title":title,"summary":summary,"content":content,"links":links,"categories":categories,"references":references}
         df = pd.DataFrame.from_records([preprocessed_dict],index="title")
+    # We append the articles one by one in case there is an error, 
+    df.to_csv(open("wikipedia2.csv",mode="w",encoding="utf-8"))
 
-    df.to_csv(open("wikipedia.csv",mode="w",encoding="utf-8"))
 
-def pipeline(iterations):
-    for i in range(iterations):
-        article = download_article()
-        print(article.title)
-        preprocess_article(article)
-
-pipeline(50)
+for i in range(10000):
+    article = download_article()
+    print(article.title)
+    title,summary,content,links,categories,references = preprocess_article(article)
+    save_article(title,summary,content,links,categories,references)
